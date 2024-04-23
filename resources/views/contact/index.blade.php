943a4fc4026dfe9d66c88db3c914ed1fd6bacf0d
emma2625 <emmanuelodobo10@gmail.com>
2024-04-23 15:21:05 +0100

    Update

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -22,14 +22,14 @@
             <!-- Outer Row -->
             <div class="row justify-content-center">
 
-                <div class="col-xl-10 col-lg-12 col-md-9 ">
+                <div class=" col-md-12 ">
 
                     <div class="card o-hidden border-0 shadow-lg my-5 ">
                         <div class="card-body p-0">
                             <!-- Nested Row within Card Body -->
                             <div class="row">
                                 <div class="col-lg-6 d-none d-lg-block ">
-                                    <img src="{{ asset('dog.jpg') }}" class="img-fluid h-100" alt="">
+                                    <img src="{{ asset('dog.jpg') }}" class="img-fluid h-100 rounded-start" style="object-fit: cover;" alt="">
                                 </div>
                                     
                                 <div class="col-lg-6 ">
@@ -42,47 +42,29 @@
 
                                             <div class="col md-2 mb-2">
                                                 <div class="mb-3">
-                                                    <label for="exampleFormControlInput1" class="form-label fw-bolder">First
-                                                        Name</label>
-                                                    <input type="text" class="form-control border border-primary"
+                                                    <label for="exampleFormControlInput1" class="form-label fw-bolder">
+                                                        Full Name
+                                                    </label>
+                                                    <input type="text" name="name" class="form-control border border-primary"
                                                         id="exampleFormControlInput1" placeholder="John">
                                                 </div>
                                                 <div class="mb-3">
                                                     <label for="exampleFormControlInput1" class="form-label fw-bolder">
-                                                        <p>Surname (Optional)</p>
+                                                        Email
                                                     </label>
-                                                    <input type="text" class="form-control border border-primary"
-                                                        id="exampleFormControlInput1" placeholder="Doe">
+                                                    <input type="email" name="email" class="form-control border border-primary"
+                                                        id="exampleFormControlInput1" placeholder="example@example.com">
                                                 </div>
-                                                <div class="mb-3">
-                                                    <label for="exampleFormControlInput1" class="form-label fw-bolder">Email
-                                                        Address</label>
-                                                    <input type="email" class="form-control border border-primary"
-                                                        id="exampleFormControlInput1" placeholder="example@gmail.com">
-                                                </div>
-                                                <div class="mb-3">
-                                                    <label for="exampleFormControlInput1" class="form-label fw-bolder">Phone Number
-                                                        (Optional)</label>
-                                                    <input type="number" class="form-control border border-primary"
-                                                        id="exampleFormControlInput1" placeholder="+234">
-                                                </div>
-                                                <select class="form-select border border-primary"
-                                                    aria-label="Default select example">
-                                                    <option selected>Reason for contact</option>
-                                                    <option value="1">Partnership</option>
-                                                    <option value="1">Careers</option>
-                                                    <option value="2">Technical Issues</option>
-                                                    <option value="3">Other</option>
-                                                </select>
+                                                
                                                 <div class="mb-3 my-3">
                                                     <label for="exampleFormControlTextarea1"
                                                         class="form-label fw-bolder">Message</label>
-                                                    <textarea class="form-control border border-primary" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                                    <textarea class="form-control border border-primary" id="exampleFormControlTextarea1" rows="5" name="message"></textarea>
                                                 </div>
 
-                                                <a href="#" class="btn btn-outline-primary rounded-pill px-5">
+                                                <button class="btn btn-outline-primary rounded-pill px-5">
                                                     Submit <i class="fa-solid fa-pencil"></i>
-                                                </a>
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
